f5aa4c3abe7b28fa1b7e806aee9e2edb1ad12df2
tettsu-dev <tettsu.dev@gmail.com>
2025-12-02 02:01:16 +0900

    add workflow

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  images: {
+    unoptimized: true
+  },
+  basePath: "/tic-tac-toe"
 };
 
 export default nextConfig;
