e774dacb4a240457a01209d21afc114631672ff4
tettsu-dev <tettsu.dev@gmail.com>
2025-12-02 09:44:12 +0900

    fix nextconfig

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,8 @@ const nextConfig: NextConfig = {
   images: {
     unoptimized: true
   },
-  basePath: "/tic-tac-toe"
+  basePath: "/tic-tac-toe",
+  assetPrefix: '/tic-tac-toe/'
 };
 
 export default nextConfig;
